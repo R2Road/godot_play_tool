4c196768f09d4dd727383d232e1b8fd7981234e9
R2Road <studior2road@gmail.com>
2024-07-16 12:09:34 +0900

    move + apply

--- project/helper_root.gd
+++ project/helper_root.gd
@@ -6,5 +6,5 @@ extends GDPTScene
 func _ready():
 	pam.set_name( "Helper Root" )
 	pam.add_split()
-	pam.add_mover( "2D Root",		Key.KEY_ESCAPE, "res://2d/2d_root.tscn" )
+	pam.add_mover( "Dev Root",		Key.KEY_ESCAPE, "res://dev_root.tscn" )
 	build_summary( eSceneType.TEST )
