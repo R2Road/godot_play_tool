82251eb764a8099ddc8ddabfadd0fe0b23087a11
R2Road <studior2road@gmail.com>
2024-07-17 13:12:15 +0900

    add scene : helper_center_pivot_with_camera2d

--- project/helper/helper_root.gd
+++ project/helper/helper_root.gd
@@ -9,4 +9,5 @@ func _ready():
 	pam.add_mover( "Dev Root",		Key.KEY_ESCAPE, "res://dev_root.tscn" )
 	pam.add_lf()
 	pam.add_mover( "Helper Center Pivot : with Viewport",	Key.KEY_1, "res://helper/helper_center_pivot_with_viewport.tscn" )
+	pam.add_mover( "Helper Center Pivot : with Camera2D",	Key.KEY_2, "res://helper/helper_center_pivot_with_camera2d.tscn" )
 	build_summary( eSceneType.TEST )
