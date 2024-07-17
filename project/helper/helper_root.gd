f4779c0b865a810d1b96b386ce524b0e7ddc9d84
R2Road <studior2road@gmail.com>
2024-07-17 13:31:11 +0900

    rename

--- project/helper/helper_root.gd
+++ project/helper/helper_root.gd
@@ -8,6 +8,6 @@ func _ready():
 	pam.add_split()
 	pam.add_mover( "Dev Root",		Key.KEY_ESCAPE, "res://dev_root.tscn" )
 	pam.add_lf()
-	pam.add_mover( "Helper Center Pivot : with Viewport",	Key.KEY_1, "res://helper/helper_center_pivot_with_viewport.tscn" )
-	pam.add_mover( "Helper Center Pivot : with Camera2D",	Key.KEY_2, "res://helper/helper_center_pivot_with_camera2d.tscn" )
+	pam.add_mover( "Center Pivot : with Viewport",	Key.KEY_1, "res://helper/helper_center_pivot_with_viewport.tscn" )
+	pam.add_mover( "Center Pivot : with Camera2D",	Key.KEY_2, "res://helper/helper_center_pivot_with_camera2d.tscn" )
 	build_summary( eSceneType.TEST )
