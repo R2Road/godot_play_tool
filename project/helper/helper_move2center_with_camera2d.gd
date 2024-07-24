09368566bde1b26bc521f10c5f7d8251c8dde7f6
R2Road <studior2road@gmail.com>
2024-07-24 15:37:56 +0900

    ...

--- project/helper/helper_move2center_with_camera2d.gd
+++ project/helper/helper_move2center_with_camera2d.gd
@@ -15,4 +15,5 @@ func _ready():
 	pam.add_back( Key.KEY_ESCAPE )
 	pam.add_lf()
 	pam.add_message( " > 빨간 원은 ( 0, 0 ) 에 위치한다." )
+	pam.add_message( " > 초록 점은 카메라의 위치다." )
 	build_summary( eSceneType.TEST )
