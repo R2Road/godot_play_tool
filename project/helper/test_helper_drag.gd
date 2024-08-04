bbdd63ca02edd4409f73a167df4fd62d333de7c9
R2Road <studior2road@gmail.com>
2024-08-04 22:33:51 +0900

    update : helper_drag : signal 추가 - signal_drag_on, signal_drag_off, signal_position_changed

--- project/helper/test_helper_drag.gd
+++ project/helper/test_helper_drag.gd
@@ -22,3 +22,14 @@ func _ready():
 	pam.add_action( "Camera L", Key.KEY_A, func(): $helper_point.position.x -= 10 )
 	pam.add_action( "Camera R", Key.KEY_D, func(): $helper_point.position.x += 10 )
 	build_summary( eSceneType.TEST )
+	
+	#
+	#
+	#
+	$Drag1/helper_drag.signal_drag_on.connect( func(): $Drag1/Status.text = "Start" )
+	
+	$Drag1/helper_drag.signal_drag_off.connect( func(): $Drag1/Status.text = "End" )
+	$Drag1/helper_drag.signal_drag_off.emit()
+	
+	$Drag1/helper_drag.signal_position_changed.connect( func( _position ): $Drag1/Position.text = str( _position ) )
+	$Drag1/helper_drag.signal_position_changed.emit( $Drag1.position )
