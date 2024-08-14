c1885118af4c4436760b14e42e4a50ef261fde70
R2Road <studior2road@gmail.com>
2024-08-15 06:34:14 +0900

    inline

--- project/addons/godot_play_tool/action/gdpt_action_manager.gd
+++ project/addons/godot_play_tool/action/gdpt_action_manager.gd
@@ -50,7 +50,57 @@ func add_mover( _name : String, _key : Key, _scene_path : String ):
 	if _scene_path.is_empty():
 		return
 		
-	container.push_back( GDPTAction.new_mover( owner, _name, _key, _scene_path ) )
+	container.push_back( GDPTAction.new(
+		_name
+		, _key
+		, func ():
+			#
+			# Scene 전환이 이미 일어났는지 확인
+			# GDPTAction은 call_deferred 로 작동한다.
+			# 키를 동시에 2개 이상 누른 경우
+			# 한 프레임에 여러번의 Scene 전환 가능
+			#
+			if owner != owner.get_tree().current_scene:
+				return
+			
+			#
+			# 현재 Scene의 Camera Off
+			#
+			if owner.get_viewport().get_camera_2d():
+				owner.get_viewport().get_camera_2d().enabled = false
+			if owner.get_viewport().get_camera_3d():
+				owner.get_viewport().get_camera_3d().enabled = false
+			
+			#
+			# 현재 Scene의 모든 개체를 제거한다.
+			#
+			var tree = owner.get_tree()
+			for c in tree.root.get_children():
+				if not c.is_in_group( "autoload" ):
+					c.queue_free()
+			
+			#
+			# SceneTree 의 change scene 을 사용하지 않고 scene 전환 효과를 만든다.
+			#
+			var next_scene = ResourceLoader.load( _scene_path ).instantiate()
+			if next_scene is GDPTScene:
+				next_scene.pam.set_last_scene( owner.pam.name, owner.scene_file_path )
+			else:
+				print_debug( "warning : is not GDPTScene" )
+			
+			#
+			# Scene 설정
+			#
+			tree.root.add_child( next_scene )
+			# current_scene 을 설정하지 않으면
+			# change_scene_to_file 을 이용한 Scene전환 이후
+			# 이전 Scene이 남아 있는 경우가 있다.
+			tree.set_current_scene( next_scene )
+			
+			# 같은 instance id 가 1번 이상 출력되면
+			# 한 프레임에 scene 전환이 여러번 수행된 것이다.
+			# print( "Last Scene Instance ID : " + str( _owner.get_instance_id() ) )
+	) )
 
 
 func add_action( _name : String, _key : Key, _functor : Callable ):
