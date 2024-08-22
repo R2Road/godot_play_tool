3633c4f8345963e1436b0ed7032de6f69fb5d47f
R2Road <studior2road@gmail.com>
2024-08-23 01:27:42 +0900

    add scene : test_action___key_color

--- project/test_action/test_action___key_color.gd
+++ project/test_action/test_action___key_color.gd
@@ -1,10 +1,10 @@
-class_name test_action___root extends GDPTScene
+class_name test_action___key_color extends GDPTScene
 
 
 
 ######################### GDPT Override ##########################
 static func scene_name()->String:
-	return "Action : Root"
+	return "Action : Key Color"
 
 
 static func scene_path()->String:
@@ -16,10 +16,9 @@ static func scene_path()->String:
 func _ready():
 	pam.set_name( scene_name() )
 	pam.add_split()
-	pam.add_lf()
-	pam.add_mover( dev_root.scene_name(),			Key.KEY_ESCAPE, dev_root.scene_path() )
-	pam.add_lf()
-	pam.add_mover( test_action___key_color.scene_name(),	Key.KEY_1, test_action___key_color.scene_path() )
-	pam.add_lf()
+	pam.add_back( Key.KEY_ESCAPE )
+	pam.add_mover( test_action___root.scene_name(),		Key.KEY_ESCAPE, test_action___root.scene_path() )
+	pam.add_action( "Q", Key.KEY_Q, func(): $Label.text = "q" )
+	pam.add_action( "W", Key.KEY_W, func(): $Label.text = "w" )
 	pam.add_split()
 	build_summary( eSceneType.ROOT )
