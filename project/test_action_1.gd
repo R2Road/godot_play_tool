1d12c436c8f9431d429dad5421362e208c7cca94
R2Road <studior2road@gmail.com>
2024-09-04 21:18:09 +0900

    ...

--- project/test_action_1.gd
+++ project/test_action_1.gd
@@ -7,6 +7,10 @@ static func scene_name()->String:
 	return "Test Action 1"
 
 
+static func scene_path()->String:
+	return super.scene_path()
+
+
 
 ############################ Override ############################
 func _ready():
