b7a5fc3e28a9753dbd6037495b6eeabc36e406e1
R2Road <studior2road@gmail.com>
2024-09-21 03:48:00 +0900

    ...

--- project/test_helper/test_helper___code.gd
+++ project/test_helper/test_helper___code.gd
@@ -17,10 +17,7 @@ func _ready():
 	pam.set_name( scene_name() )
 	pam.add_split()
 	pam.add_back( Key.KEY_ESCAPE )
-	pam.add_lf()
-	pam.add_action( "GDPT Debug Flag ON", 	Key.KEY_1, 		func(): GDPT.on_debug = true )
-	pam.add_action( "GDPT Debug Flag OFF", 	Key.KEY_2, 		func(): GDPT.on_debug = false )
-	pam.add_action( "Debug Print", 			Key.KEY_SPACE, 	func(): GDPT._debug_print( func()->String: return "Test Message" ) )
+	pam.add_split()
 	build_summary( eSceneType.TEST )
 	
 	$helper_code_1.show_code( scene_path().replace( ".tscn", ".gd" ), 6, 7 )
