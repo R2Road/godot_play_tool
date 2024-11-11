0e1d5dc8c82ebafc0f0ef0c125473c425f4a29a2
R2Road <studior2road@gmail.com>
2024-11-11 13:53:50 +0900

    update : helper_log : RichTextLabel 대신 ScrollContainer + VBoxContainer 사용

--- project/addons/godot_play_tool/helper/helper_log.gd
+++ project/addons/godot_play_tool/helper/helper_log.gd
@@ -3,10 +3,10 @@ class_name helper_log extends MarginContainer
 
 
 ### OnReady ######################################################
-@onready var rich_text_label : RichTextLabel = $RichTextLabel
-
 @onready var scroll_container : ScrollContainer = $ScrollContainer
 @onready var vbox_container : VBoxContainer = $ScrollContainer/VBoxContainer
+
+@onready var label : Label = $Label
 
 
 
@@ -16,11 +16,11 @@ func _ready()->void:
 	scroll_bar.changed.connect( _on_scroll_bar_changed )
 
 
+
 ### Interface ####################################################
 func add_normal( message : String )->void:
-	rich_text_label.add_text( message + str( rich_text_label.get_line_count() ) + "\n" )
-	var l = Label.new()
-	l.text = "> asdf\nasdfasdf" + str( rich_text_label.get_line_count() )
+	var l = label.duplicate()
+	l.text = message
 	vbox_container.add_child( l )
 
 
