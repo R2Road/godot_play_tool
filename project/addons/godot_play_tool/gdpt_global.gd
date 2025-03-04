8f984d0050053284bf6dd52d7a27fdd7bbaa9ed3
R2Road <studior2road@gmail.com>
2025-03-04 15:41:30 +0900

    const

--- project/addons/godot_play_tool/gdpt_global.gd
+++ project/addons/godot_play_tool/gdpt_global.gd
@@ -8,30 +8,30 @@ var version : String
 var on_debug = true
 var on_assert = true
 
-var scene_helper = preload( "res://addons/godot_play_tool/scene/gdpt_scene_helper.gd" )
+const scene_helper = preload( "res://addons/godot_play_tool/scene/gdpt_scene_helper.gd" )
 
 # 
-var helper_circle_scene = preload( "res://addons/godot_play_tool/helper/helper_circle.tscn" )
-var helper_drag_scene = preload( "res://addons/godot_play_tool/helper/helper_drag.tscn" )
-var helper_line_scene = preload( "res://addons/godot_play_tool/helper/helper_line.tscn" )
-var helper_move2center_scene = preload( "res://addons/godot_play_tool/helper/helper_move2center.tscn" )
-var helper_point_scene = preload( "res://addons/godot_play_tool/helper/helper_point.tscn" )
-var helper_rect_scene = preload( "res://addons/godot_play_tool/helper/helper_rect.tscn" )
-var helper_reticle_scene = preload( "res://addons/godot_play_tool/helper/helper_reticle.tscn" )
-var helper_0_0_scene = preload( "res://addons/godot_play_tool/helper/helper_0_0.tscn" )
+const helper_circle_scene = preload( "res://addons/godot_play_tool/helper/helper_circle.tscn" )
+const helper_drag_scene = preload( "res://addons/godot_play_tool/helper/helper_drag.tscn" )
+const helper_line_scene = preload( "res://addons/godot_play_tool/helper/helper_line.tscn" )
+const helper_move2center_scene = preload( "res://addons/godot_play_tool/helper/helper_move2center.tscn" )
+const helper_point_scene = preload( "res://addons/godot_play_tool/helper/helper_point.tscn" )
+const helper_rect_scene = preload( "res://addons/godot_play_tool/helper/helper_rect.tscn" )
+const helper_reticle_scene = preload( "res://addons/godot_play_tool/helper/helper_reticle.tscn" )
+const helper_0_0_scene = preload( "res://addons/godot_play_tool/helper/helper_0_0.tscn" )
 
 # Input
-var helper_input_packed_scene = preload( "res://addons/godot_play_tool/helper/helper_input.tscn" )
+const helper_input_packed_scene = preload( "res://addons/godot_play_tool/helper/helper_input.tscn" )
 
 # U.I
-var helper_hslider_scene = preload( "res://addons/godot_play_tool/helper/helper_hslider.tscn" )
+const helper_hslider_scene = preload( "res://addons/godot_play_tool/helper/helper_hslider.tscn" )
 
 # Pragma
-var helper_code_scene = preload( "res://addons/godot_play_tool/helper/helper_code.tscn" )
-var helper_output_scene = preload( "res://addons/godot_play_tool/helper/helper_output.tscn" )
+const helper_code_scene = preload( "res://addons/godot_play_tool/helper/helper_code.tscn" )
+const helper_output_scene = preload( "res://addons/godot_play_tool/helper/helper_output.tscn" )
 
 # Tool
-var helper_controller_script = preload( "res://addons/godot_play_tool/helper/helper_controller.gd" )
+const helper_controller_script = preload( "res://addons/godot_play_tool/helper/helper_controller.gd" )
 
 
 
