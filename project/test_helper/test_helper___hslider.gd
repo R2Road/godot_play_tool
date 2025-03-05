daf50e0dc99ee936bdc1ac2ea24167246a6ee56c
R2Road <studior2road@gmail.com>
2025-03-05 21:43:06 +0900

    add scene : test_helper___hslider

---
+++ project/test_helper/test_helper___hslider.gd
@@ -0,0 +1,20 @@
+class_name test_helper___hslider extends GDPTScene
+
+
+
+### GDPT Override ################################################
+static func scene_name()->String:
+	return "helper_hslider"
+
+static func scene_path()->String:
+	return super.scene_path()
+
+
+
+### Override #####################################################
+func _ready():
+	pam.set_name( scene_name() )
+	pam.add_split()
+	pam.add_back( Key.KEY_ESCAPE )
+	pam.add_split()
+	build_summary( eSceneType.TEST )
