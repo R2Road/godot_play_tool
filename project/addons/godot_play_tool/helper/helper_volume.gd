2f978377aa45836628dfc71d90d619e8baa4f465
R2Road <studior2road@gmail.com>
2025-03-06 19:36:42 +0900

    rename function

--- project/addons/godot_play_tool/helper/helper_volume.gd
+++ project/addons/godot_play_tool/helper/helper_volume.gd
@@ -46,11 +46,11 @@ func _ready()->void:
 
 ### Interface ####################################################
 func set_volume( _percentage : float ) -> void:
-	update_volume( _percentage )
+	apply_volume( _percentage )
 	slider.value = _percentage
 
 
-func update_volume( _percentage : float ) -> void:
+func apply_volume( _percentage : float ) -> void:
 	var scale : float = 20
 	var divisor : float = 50
 	
@@ -64,5 +64,5 @@ func update_volume( _percentage : float ) -> void:
 
 ### Signal : Receiver ############################################
 func _on_helper_hslider_value_changed( _percentage : float ) -> void:
-	update_volume( _percentage )
+	apply_volume( _percentage )
 	
