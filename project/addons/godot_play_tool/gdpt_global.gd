1c6814939f65ce81bc14f597bf6cea6aa6ffdcc2
R2Road <studior2road@gmail.com>
2025-03-08 03:16:49 +0900

    update gdpt_global

--- project/addons/godot_play_tool/gdpt_global.gd
+++ project/addons/godot_play_tool/gdpt_global.gd
@@ -31,6 +31,9 @@ const helper_volume_packed_scene = preload( "res://addons/godot_play_tool/helper
 const helper_code_packed_scene = preload( "res://addons/godot_play_tool/helper/helper_code.tscn" )
 const helper_output_packed_scene = preload( "res://addons/godot_play_tool/helper/helper_output.tscn" )
 
+# ETC
+const helper_scene_change_packed_scene = preload( "res://addons/godot_play_tool/helper/helper_scene_change.tscn" )
+
 # Tool
 const helper_controller_packed_script = preload( "res://addons/godot_play_tool/helper/helper_controller.gd" )
 
