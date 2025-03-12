801cfb374fd49c718719990d076d2f9cccd9f68f
R2Road <studior2road@gmail.com>
2025-03-12 14:37:41 +0900

    split scene

---
+++ project/test_helper/test_helper___move2center_root.gd
@@ -0,0 +1,27 @@
+class_name test_helper___move2center_root extends GDPTScene
+
+
+
+### GDPT Override ################################################
+static func scene_name()->String:
+	return "Move2Center : Root"
+
+
+static func scene_path()->String:
+	return super.scene_path()
+
+
+
+### Override #####################################################
+func _ready():
+	pam.set_name( scene_name() )
+	pam.add_split()
+	pam.add_front( test_helper_root.scene_name(),			Key.KEY_ESCAPE, test_helper_root.scene_path() )
+	pam.add_lf()
+	pam.add_mover( test_helper___move2center_with_mainscene.scene_name(),	Key.KEY_1, test_helper___move2center_with_mainscene.scene_path() )
+	pam.add_mover( test_helper___move2center_with_viewport.scene_name(),	Key.KEY_2, test_helper___move2center_with_viewport.scene_path() )
+	pam.add_mover( test_helper___move2center_with_camera2d.scene_name(),	Key.KEY_3, test_helper___move2center_with_camera2d.scene_path() )
+	pam.add_mover( test_helper___move2center_with_canvaslayer.scene_name(),	Key.KEY_4, test_helper___move2center_with_canvaslayer.scene_path() )
+	pam.add_mover( test_helper___move2center_with_control.scene_name(),		Key.KEY_5, test_helper___move2center_with_control.scene_path() )
+	pam.add_lf()
+	build_summary( eSceneType.ROOT )
