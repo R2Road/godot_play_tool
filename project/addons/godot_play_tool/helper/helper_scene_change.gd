5fd9a7f046f9c015a60d4ec1af7bd8f3b99bf451
R2Road <studior2road@gmail.com>
2025-03-14 12:08:59 +0900

    update helper : helper_scene_change : 이전 scene이 삭제된 후 새 scene 추가.

--- project/addons/godot_play_tool/helper/helper_scene_change.gd
+++ project/addons/godot_play_tool/helper/helper_scene_change.gd
@@ -2,6 +2,16 @@ class_name GDPTHelper_SceneChange extends CanvasLayer
 
 
 
+### Summary #####################################################
+#
+# global 객체로 추가해서 사용하는 것이 아닌 경우.
+# > 이전 scene의 _exit_tree 가 현재 scene의 _enter_tree보다 늦게 불린다.
+# > 해당 함수를 사용 한다면 주의 할 것.
+#
+#################################################################
+
+
+
 ### Export #######################################################
 @export var color_rect_node : ColorRect
 
@@ -20,7 +30,9 @@ enum eStep
 	FADE_OUT_WAIT,
 	
 	LOAD,
-	CHANGE,
+	REMOVE_LAST_SCENE,
+	REMOVE_LAST_SCENE_WAIT,
+	CHANGE_SCENE,
 	
 	FADE_IN_START,
 	FADE_IN_WAIT,
@@ -59,11 +71,17 @@ func _physics_process(delta: float) -> void:
 		
 		eStep.LOAD:
 			next_scene = ResourceLoader.load( scene_path ).instantiate()
-			current_step = eStep.CHANGE
+			current_step = eStep.REMOVE_LAST_SCENE
 		
-		eStep.CHANGE:
+		eStep.REMOVE_LAST_SCENE:
 			remove_last_scene()
-			change_scene()
+			
+			#
+			# helper_scene_change가 global 객체가 아니라면 eStep.CHANGE_SCENE에 도달하지 못한다.
+			# 즉시 scene 변경 처리를 마무리 한다.
+			#
+			if get_tree().root != get_parent():
+				change_scene()
 			
 			#
 			# helper_scene_change의 Z 값을 변경해서 Fade-in 처리를 정상적으로 마무리 한다.
@@ -71,6 +89,13 @@ func _physics_process(delta: float) -> void:
 			if get_tree().root == self.get_parent():
 				get_tree().root.move_child( self, get_tree().root.get_child_count() )
 			
+			current_step = eStep.REMOVE_LAST_SCENE_WAIT
+		eStep.REMOVE_LAST_SCENE_WAIT:
+			if auto_load_count == get_tree().root.get_child_count():
+				current_step = eStep.CHANGE_SCENE
+		
+		eStep.CHANGE_SCENE:
+			change_scene()
 			current_step = eStep.FADE_IN_START
 		
 		eStep.FADE_IN_START:
@@ -106,7 +131,7 @@ func start( _scene_path : String, _fade_out_seconds : float = 0.8, _fade_in_seco
 	
 	auto_load_count = 0
 	for c in get_tree().root.get_children():
-		if not c.is_in_group( "autoload" ):
+		if c.is_in_group( "autoload" ):
 			auto_load_count += 1
 	
 	current_step = eStep.FADE_OUT_START
@@ -139,3 +164,5 @@ func change_scene()->void:
 	# change_scene_to_file 을 이용한 Scene전환 이후
 	# 이전 Scene이 남아 있는 경우가 있다.
 	get_tree().set_current_scene( next_scene )
+	
+	next_scene = null
