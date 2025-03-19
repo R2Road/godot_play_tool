a0463be9c5a2727bdc8efb15f27dc0cd280cc62c
R2Road <studior2road@gmail.com>
2025-03-19 23:49:24 +0900

    preload 지정된 scene을 addon에서 global 객체로 등록하면 "순환 로드(?)" 문제가 생긴다.
    에러 메세지
    ERROR: Another resource is loaded from path 'res://addons/godot_play_tool/helper/helper_change_scene.tscn' (possible cyclic resource inclusion)

--- project/addons/godot_play_tool/gdpt_global.gd
+++ project/addons/godot_play_tool/gdpt_global.gd
@@ -32,7 +32,7 @@ const helper_code_packed_scene = preload( "res://addons/godot_play_tool/helper/h
 const helper_output_packed_scene = preload( "res://addons/godot_play_tool/helper/helper_output.tscn" )
 
 # ETC
-const helper_scene_change_packed_scene = preload( "res://addons/godot_play_tool/helper/helper_scene_change.tscn" )
+var helper_scene_change_packed_scene = ResourceLoader.load( "res://addons/godot_play_tool/helper/helper_scene_change.tscn" )
 
 # Tool
 const helper_controller_packed_script = preload( "res://addons/godot_play_tool/helper/helper_controller.gd" )
